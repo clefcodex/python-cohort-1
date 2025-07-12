bfe3f1879fef255af83fcedb604467d2f74239b1
clefcodex <iwewale1@gmail.com>
2025-07-12 10:47:22 +0100

    next commit

---
+++ module_1/00_intro.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "a22dbf4d",
+   "metadata": {},
+   "source": [
+    "Comments (#) –  Comments are like sticky notes in your code. They start with # and are ignored by Python, so they’re just for you or others to understand what the code does.<br>\n",
+    "Example: # This calculates your age<br>\n",
+    "Indentation (Spaces/Tabs) – Python uses them to group code (like paragraphs in a book).<br>\n",
+    "Code Blocks – Sections of code that belong together (e.g., inside loops or functions).<br>\n",
+    "Keywords – Special words Python reserves (e.g., if, for, while). Don’t use\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
---
+++ module_1/lesson1.py
@@ -0,0 +1,84 @@
+print("Hello")
+
+
+
+
+{
+    "name": "Token D",
+    "symbol": "TKD",
+    "blockchain": "Core Chain",
+    "current_price_usd": 200.0,
+    "transaction_history": [
+        {
+            "date": "2024-01-10",
+            "prices": {
+                "open": 198.0,
+                "close": 200.0,
+                "high": 202.0,
+                "low": 196.0
+            },
+            "volume": 18000
+        },
+        {
+            "date": "2024-01-09",
+            "prices": {
+                "open": 196.0,
+                "close": 198.0,
+                "high": 199.0,
+                "low": 195.0
+            },
+            "volume": 17000
+        }
+    ],
+    "supported_chains": ["Core", "Binance Smart Chain", "Avalanche"]
+}
+
+
+
+
+
+[
+    {
+        'name': 'Token A', 
+        'symbol': 'TKA', 
+        'blockchain': 'Ethereum', 
+        'current_price_usd': 100.0, 
+        'transaction_history': [{'date': '2024-01-10', 
+                                 'prices': {'open': 98.0, 'close': 100.0, 'high': 101.0, 'low': 97.0}, 'volume': 12000}, 
+                                {'date': '2024-01-09', 
+                                 'prices': {'open': 97.0, 'close': 98.0, 'high': 99.0, 'low': 96.0}, 'volume': 15000}], 
+                                 'supported_chains': ['Ethereum', 'Polygon']
+    }, 
+    {
+        'name': 'Token B', 
+        'symbol': 'TKB', 
+        'blockchain': 'Binance Smart Chain', 
+        'current_price_usd': 200.0, 
+        'transaction_history': [{'date': '2024-01-10', 
+                                 'prices': {'open': 198.0, 'close': 200.0, 'high': 202.0, 'low': 196.0}, 'volume': 18000}, 
+                                 {'date': '2024-01-09', 
+                                'prices': {'open': 196.0, 'close': 198.0, 'high': 199.0, 'low': 195.0}, 'volume': 17000}], 
+                                'supported_chains': ['Binance Smart Chain', 'Avalanche']
+    }, 
+    { 
+        'name': 'Token C', 
+        'symbol': 'TKC', 
+        'blockchain': 'Solana', 
+        'current_price_usd': 300.0, 
+        'transaction_history': [{'date': '2024-01-10', 
+                                'prices': {'open': 295.0, 'close': 300.0, 'high': 302.0, 'low': 294.0}, 'volume': 22000}, 
+                            {'date': '2024-01-09', 
+                             'prices': {'open': 294.0, 'close': 295.0, 'high': 296.0, 'low': 293.0}, 'volume': 21000}], 
+                             'supported_chains': ['Solana', 'Fantom']
+    }, 
+    {
+        'name': 'Token D', 
+        'symbol': 'TKD', 
+        'blockchain': 'Core Chain', 
+        'current_price_usd': 200.0, 
+        'transaction_history': [{'date': '2024-01-10', 
+                                 'prices': {'open': 198.0, 'close': 200.0, 'high': 202.0, 'low': 196.0}, 'volume': 18000}, 
+                                 {'date': '2024-01-09', 
+                                  'prices': {'open': 196.0, 'close': 198.0, 'high': 199.0, 'low': 195.0}, 'volume': 17000}], 
+                                  'supported_chains': ['Core', 'Binance Smart Chain', 'Avalanche']
+    },]
